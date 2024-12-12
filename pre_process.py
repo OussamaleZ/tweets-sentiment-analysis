f5bc1fcb8977d79f863f379d8cb5c7eaa7be460a
ZOUHRY Oussama <167140266+OussamaleZ@users.noreply.github.com>
2024-12-12 14:31:58 +0100

    Add files via upload

--- pre_process.py
+++ pre_process.py
@@ -16,13 +16,19 @@ from sklearn.model_selection import train_test_split
 # Download some NLP models for processing, optional
 nltk.download('stopwords')
 nltk.download('wordnet')
+
 stop_words = set(stopwords.words('english')) - {"not", "never", "no"}
-custom_stopwords = {
-    "fifaworldcup", "argentina", "brazil", "france", "germany", 
+
+countries = {
+    "argentina", "brazil", "france", "germany", 
     "netherlands", "usa", "croatia", "chile", "belgium", "australia",
     "spain","cameroun","brazil","united","states","honduras","switzerland",
     "croatia","mexico","netherlands","portugal","ghana","slovenia",
-    "greece","ivory","coast","world","worldcupfinal","worldcup","cup"
+    "greece","ivory","coast","algeria","serbia","nigeria","south","korea","southkorea",
+}
+
+custom_stopwords = {
+    "fifaworldcup", "world","fifa"
 }
 
 
@@ -32,8 +38,6 @@ def extract_usernames(tweet):
     match = re.search(r'@\w+', tweet)
     return match.group(0) if match else None
 
-
-
 def preprocess_text(text):
     # Lowercasing
     text = str(text).lower()    
@@ -41,34 +45,47 @@ def preprocess_text(text):
     text = re.sub(r"#\w+", '', text)
     # Remove mentions
     text = re.sub(r"@\w+", '', text) 
-    # Remove punctuation
-    text = re.sub(r'[^\w\s]', '', text) 
-    # Remove numbers
-    text = re.sub(r'\d+', '', text) 
     # Remove URLs
-    text = re.sub(r"http\S+|www\S+", '', text)     
-    # Step 1: Remove 'RT' at the beginning
-    text = re.sub(r'^rt\s+', '', text)
+    text = re.sub(r"http\S+|www\S+", '', text)    
+    # Remove punctuation
+    # text = re.sub(r'[^\w\s]', '', text)
+    #Replace " vs " with ""
+    text = re.sub(r"\b vs \b", "", text)
+    # Remove numbers
+    # text = re.sub(r'\d+', '', text) 
+    # Remove 'RT' at the beginning
+    # text = re.sub(r'^rt\s+', '', text)
     # Remove punctuation
     text = re.sub(r"[^\w\s]", '', text)  
     # Remove elongated words
     text = re.sub(r'(.)\1{2,}', r'\1\1', text)
-
+    # Replace '  ' by ' ' (Remove multiple spaces)
+    text = re.sub(r' +', ' ', text)
+    #Remove countries with the string ""
+    # text = re.sub(r'\b(?:{})\b'.format('|'.join(countries)), '', text)
+    # Replace single-letter words with an empty string
+    pattern = r"\b[a-zA-Z]\b"
+    # text = re.sub(pattern, "", text)
+    # Remove extra spaces that might be left behind
+    text = re.sub(r"\s+", " ", text).strip()
+    #Replace "im" with "I am"
+    text = re.sub(r"\bim\b", "I am", text)
+    
     # Tokenization
     words = text.split()  
 
-    words = [word for word in words if word not in stop_words]
-    words = [word for word in words if word not in custom_stopwords]
+    # words = [word for word in words if word not in stop_words]
+    # words = [word for word in words if word not in custom_stopwords]
     # Lemmatization
-    lemmatizer = WordNetLemmatizer()
-    words = [lemmatizer.lemmatize(word) for word in words]
+    # lemmatizer = WordNetLemmatizer()
+    # words = [lemmatizer.lemmatize(word) for word in words]
 
     return ' '.join(words)
 
 
 def preprocess():
     # Ensure the output directory exists
-    output_dir = "eval_tweets_preprocessed"
+    output_dir = "eval_tweets_preprocessed_soft"
     os.makedirs(output_dir, exist_ok=True)
 
     # Iterate over all files in the eval_tweets directory
@@ -96,5 +113,5 @@ def preprocess():
         output_path = os.path.join(output_dir, filename)
         df.to_csv(output_path, index=False)
 
-print(preprocess_text("RT @NASA: Learning about aerodynamics is our GOOOOOOAAAL! Check out our tests of the #WorldCup ball: http://t.co/HgGv1FDRFf http://t.co/9fn…"))
+print(preprocess_text("RT @_theworldcup: If Germany win tonight we will give this GOLD iPhone5 away to 1 lucky winner just RT & FOLLOW @_theworldcup to enter http…"))
 preprocess()
